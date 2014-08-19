c7794986f1257c0b391b26a2b5f130c0fd4366cf
hytcchenqiuyang <874376118@qq.com>
2014-08-19 16:49:30 +0800

    taobao1.3

---
+++ taobao/js/yimai.js
@@ -0,0 +1,38 @@
+$(function(){
+	/*alert("d");*/
+	$("#box1").hover(function () {
+		$("#operate0").show();	
+	},function(){
+		$("#operate0").hide();	
+	})
+
+	$("#box2").hover(function () {
+		$("#operate2").show();	
+	},function(){
+		$("#operate2").hide();	
+	})
+
+	$("#box3").hover(function () {
+		$("#operate3").show();	
+	},function(){
+		$("#operate3").hide();	
+	})
+
+	$("#box4").hover(function () {
+		$("#operate4").show();	
+	},function(){
+		$("#operate4").hide();	
+	})
+
+	$("#box5").hover(function () {
+		$("#operate5").show();	
+	},function(){
+		$("#operate5").hide();	
+	})
+
+	$("#box6").hover(function () {
+		$("#operate6").show();	
+	},function(){
+		$("#operate6").hide();	
+	})
+});
